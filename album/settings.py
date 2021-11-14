70a224bb73c8e15fcf337ac5a3405fadc162c0f5
hwn <houweinuo@163.com>
2021-11-14 18:48:58 +0800

    1

--- album/settings.py
+++ album/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_&!ms7%hou9b0x9hk2)yxtu-=r!&!71u50h%ax7*lpq%w-n*b^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
